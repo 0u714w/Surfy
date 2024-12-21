6b51ee7fe9217c7ca65176bd47c2e889f7e37ac0
Doug Enas <douglassenas@gmail.com>
2024-12-21 07:23:45 -0500

    surfy.2

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/vue-surf-app/' : '/',
-    devServer: {
-        port: 8080,
-        open: true
-    }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-surf-app/'
+    : '/',
+  devServer: {
+    port: 8080,
+    open: true
+  }
 };
